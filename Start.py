c2fb7cf1b7506f7b6dff856ac6dfa86dfa82799c
4ndertron <robert.anderson@vivintsolar.com>
2019-04-11 13:55:26 -0600

    Added two more ad-hoc todo's

--- Start.py
+++ Start.py
@@ -1,6 +1,10 @@
 print('Let the games, begin!')
 
-# TODO: TURN THE DRIVERS INTO A 5 || 10 COLUMN GRAPH (After adjustment)
+# TODO: Only count deceased lost with accounts that have the codes in the LD system.
+
+# TODO: Replace the Net bar graph with a pie chart of Marche's inflow by priority bucket.
+
+# TODO: TURN THE DRIVERS INTO A 5 || 10 COLUMN GRAPH (After adjustment & rerun)
 
 # TODO: Make a daily csv report that gets sent to the appropriate managers and supervisors
 #  for the urgent feedback from filings.
