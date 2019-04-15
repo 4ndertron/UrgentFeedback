29aa8de56077643f4a8e673659265545c24f686d
4ndertron <robert.anderson@vivintsolar.com>
2019-04-15 09:28:36 -0600

    Updated two ad-hoc requests for the Risk Management Business Review report.

--- Start.py
+++ Start.py
@@ -1,6 +1,8 @@
 print('Let the games, begin!')
 
-# TODO:
+# TODO: Update the social media goal... I can't
+
+# TODO: Turn the ERT Financial impact table into a stacked trending graph...
 
 # TODO: Make a daily csv report that gets sent to the appropriate managers and supervisors
 #  for the urgent feedback from filings.
