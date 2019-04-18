3888e0d7061d39ad03e8a819b2e4937eea6ff223
4ndertron <robert.anderson@vivintsolar.com>
2019-04-18 10:23:23 -0600

    Saving progress for a computer restart.

--- SQL/Attrition/CX Attrition.sql
+++ SQL/Attrition/CX Attrition.sql
@@ -1,6 +1,7 @@
 WITH ENTIRE_HISTORY AS (
     SELECT HR.EMPLOYEE_ID
          , HR.SUPERVISORY_ORG
+         , ANY_VALUE(COST_CENTER)                                                  AS COST_CENTER
          , ANY_VALUE(HR.FULL_NAME)                                                 AS FULL_NAME
          , ANY_VALUE(HR.SUPERVISOR_NAME_1)                                         AS SUPERVISOR_NAME_1
          , MIN(HR.CREATED_DATE)                                                    AS TEAM_START_DATE
@@ -11,6 +12,7 @@ WITH ENTIRE_HISTORY AS (
                     NVL(ANY_VALUE(HR.TERMINATION_DATE), CURRENT_DATE()))           AS MONTH_TENURE
          , ANY_VALUE(HR.TERMINATION_CATEGORY)                                      AS TERMINATION_CATEGORY
          , ANY_VALUE(HR.TERMINATION_REASON)                                        AS TERMINATION_REASON
+         -- Begin custom fields in the table
          , ROW_NUMBER() OVER(PARTITION BY HR.EMPLOYEE_ID ORDER BY TEAM_START_DATE) AS RN
          , CASE
                WHEN ANY_VALUE(HR.SUPERVISORY_ORG) IN
@@ -20,13 +22,15 @@ WITH ENTIRE_HISTORY AS (
                      'Customer Support', 'Default Managers', 'Executive Resolutions', 'Inbound', 'PIO Ops', 'PMO/BA',
                      'Project Specialists', 'Scheduling', 'Solutions', 'Training', 'Transfers',
                      'WFM', 'Workforce Management', 'E-mail Administration') THEN TRUE
+               WHEN ANY_VALUE(HR.COST_CENTER) IN ('Solar Customer Experience', 'Solar Performance Corp') THEN TRUE
                ELSE FALSE END                                                      AS DIRECTOR_ORG
          , LEAD(DIRECTOR_ORG, 1, DIRECTOR_ORG) OVER(PARTITION BY HR.EMPLOYEE_ID
                 ORDER BY TEAM_START_DATE)                                          AS NEXT_DIRECTOR
          , CASE
-               WHEN RN = 1 AND DIRECTOR_ORG THEN TRUE
                WHEN DIRECTOR_ORG = NEXT_DIRECTOR THEN FALSE
-               ELSE TRUE END                                                       AS TRANSFER_FLAG
+               WHEN DIRECTOR_ORG AND TERMINATION_DATE IS NOT NULL THEN TRUE
+               ELSE TRUE END                                                       AS TRANSFER_OUT
+--     , NVL(ANY_VALUE(HR.TERMINATION_DATE),TEAM_EXPIRY_DATE) AS TRANSFER_OUT_DATE
     FROM HR.T_EMPLOYEE_ALL AS HR
     GROUP BY HR.EMPLOYEE_ID
            , HR.SUPERVISORY_ORG
@@ -37,7 +41,7 @@ WITH ENTIRE_HISTORY AS (
    , TRANSFER_HISTORY AS (
     SELECT *
     FROM ENTIRE_HISTORY
-    WHERE TRANSFER_FLAG
+    WHERE TRANSFER_OUT
 )
 
    , CORE_TABLE AS (
--- SQL/Attrition/scribbles.sql
+++ SQL/Attrition/scribbles.sql
@@ -1,33 +1,42 @@
 WITH ENTIRE_HISTORY AS (
-    SELECT WH.BADGE_ID
-         , ANY_VALUE(WH.FIRST_NAME) || ' ' || ANY_VALUE(WH.LAST_NAME)      AS FULL_NAME
-         , MIN(WH.WEEK_START)                                              AS START_WEEK
-         , MAX(WH.WEEK_END)                                                AS END_WEEK
-         , ANY_VALUE(WH.TEAM)                                              AS TEAM
-         , WH.ORG
-         , ANY_VALUE(WH.COST_CENTER)                                       AS COST_CODE
-         , ROW_NUMBER() OVER(PARTITION BY WH.BADGE_ID ORDER BY START_WEEK) AS RN
+    SELECT HR.EMPLOYEE_ID
+         , HR.SUPERVISORY_ORG
+         , ANY_VALUE(COST_CENTER)                                                  AS COST_CENTER
+         , ANY_VALUE(HR.FULL_NAME)                                                 AS FULL_NAME
+         , ANY_VALUE(HR.SUPERVISOR_NAME_1)                                         AS SUPERVISOR_NAME_1
+         , MIN(HR.CREATED_DATE)                                                    AS TEAM_START_DATE
+         , MAX(HR.EXPIRY_DATE)                                                     AS TEAM_EXPIRY_DATE
+         , ANY_VALUE(HR.HIRE_DATE)                                                 AS HIRE_DATE
+         , ANY_VALUE(HR.TERMINATION_DATE)                                          AS TERMINATION_DATE
+         , DATEDIFF('MM', ANY_VALUE(HR.HIRE_DATE),
+                    NVL(ANY_VALUE(HR.TERMINATION_DATE), CURRENT_DATE()))           AS MONTH_TENURE
+         , ANY_VALUE(HR.TERMINATION_CATEGORY)                                      AS TERMINATION_CATEGORY
+         , ANY_VALUE(HR.TERMINATION_REASON)                                        AS TERMINATION_REASON
+         -- Begin custom fields in the table
+         , ROW_NUMBER() OVER(PARTITION BY HR.EMPLOYEE_ID ORDER BY TEAM_START_DATE) AS RN
          , CASE
-               WHEN ANY_VALUE(WH.TEAM) IN
+               WHEN ANY_VALUE(HR.SUPERVISORY_ORG) IN
                     ('Business Analytics', 'Call & Workflow Quality Control', 'Central Scheduling', 'Click Support',
                      'Customer Experience', 'Customer Relations', 'Customer Service', 'Customer Solutions',
                      'Customer Success', 'Customer Success 1', 'Customer Success I', 'Customer Success II',
                      'Customer Support', 'Default Managers', 'Executive Resolutions', 'Inbound', 'PIO Ops', 'PMO/BA',
-                     'Project Specialists', 'Sales Concierge', 'Scheduling', 'Solutions', 'Training', 'Transfers',
-                     'WFM', 'Workforce Management', 'E-mail Administration')
-                   AND COST_CODE LIKE ('%Customer Experience%', '%Solar Performance Corp%') THEN TRUE
-               ELSE FALSE END                                              AS DIRECTOR_ORG
-         , LEAD(DIRECTOR_ORG, 1, DIRECTOR_ORG) OVER(PARTITION BY WH.BADGE_ID
-                ORDER BY START_WEEK)                                       AS NEXT_DIRECTOR
+                     'Project Specialists', 'Scheduling', 'Solutions', 'Training', 'Transfers',
+                     'WFM', 'Workforce Management', 'E-mail Administration') THEN TRUE
+               WHEN ANY_VALUE(HR.COST_CENTER) IN ('Solar Customer Experience', 'Solar Performance Corp') THEN TRUE
+               ELSE FALSE END                                                      AS DIRECTOR_ORG
+         , LEAD(DIRECTOR_ORG, 1, DIRECTOR_ORG) OVER(PARTITION BY HR.EMPLOYEE_ID
+                ORDER BY TEAM_START_DATE)                                          AS NEXT_DIRECTOR
          , CASE
-               WHEN RN = 1 AND DIRECTOR_ORG THEN TRUE
                WHEN DIRECTOR_ORG = NEXT_DIRECTOR THEN FALSE
-               ELSE TRUE END                                               AS TRANSFER_FLAG
-    FROM D_POST_INSTALL.T_WORKDAY_HISTORY AS WH
-    GROUP BY WH.BADGE_ID
-           , WH.ORG
-    ORDER BY WH.BADGE_ID
-           , START_WEEK
+               WHEN DIRECTOR_ORG AND TERMINATION_DATE IS NOT NULL THEN TRUE
+               ELSE TRUE END                                                       AS TRANSFER_OUT
+--     , NVL(ANY_VALUE(HR.TERMINATION_DATE),TEAM_EXPIRY_DATE) AS TRANSFER_OUT_DATE
+    FROM HR.T_EMPLOYEE_ALL AS HR
+    GROUP BY HR.EMPLOYEE_ID
+           , HR.SUPERVISORY_ORG
+    ORDER BY HR.EMPLOYEE_ID
+           , TEAM_START_DATE
 )
+
 SELECT *
 FROM ENTIRE_HISTORY
