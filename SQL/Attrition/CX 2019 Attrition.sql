2cd399774082fef044b310ba9374c658299e2366
4ndertron <robert.anderson@vivintsolar.com>
2019-04-18 12:38:56 -0600

    The final iteration of the core table has been completed. I am now moving the core table into the entire query for the graph formats.

---
+++ SQL/Attrition/CX 2019 Attrition.sql
@@ -0,0 +1,33 @@
+-- Research
+-- ========
+-- Where TERMINATED is true, TERMINATION_DATE, TERMINATION_CATEGORY, and TERMINATION_REASON are always filled
+-- TERMINATION_COMPLETE_DATE can be filled when TERMINATED is false and null when TERMINATED is true
+-- Only 3.7% of the time. Generally, TERMINATION_COMPLETE_DATE matches nullness of TERMINATION_DATE
+-- Dates can be different though...
+-- Looks like logic on TERMINATION_COMPLETED_DATE transcends the first row it applied to (it can be retroactively applied)
+-- That is, it each EMPLOYEE_ID on HR.T_EMPLOYEE_ALL has only one distinct TERMINATION_COMPLETE_DATE
+-- **Probably best to trust TERMINATION_DATE only**
+-- Reached out to Jed's team via email to get distinction
+
+-- Get termination dates and reasons for our org, YTD
+SELECT employee_id
+     , full_name
+     , supervisor_name_1
+     , supervisory_org
+    //
+     , terminated
+     , termination_date
+    //
+     , termination_complete_date
+     , termination_category
+     , termination_reason
+    //
+     , created_date
+    //
+     , expiry_date
+FROM hr.t_employee
+WHERE mgr_id_3 = 209122 -- Chuck Browne
+  AND terminated
+  AND termination_date >= '2019-01-01'
+ORDER BY termination_date ASC
+;
--- SQL/Attrition/scribbles.sql
+++ SQL/Attrition/scribbles.sql
@@ -4,15 +4,18 @@ WITH ENTIRE_HISTORY AS (
          , ANY_VALUE(COST_CENTER)                                                  AS COST_CENTER
          , ANY_VALUE(HR.FULL_NAME)                                                 AS FULL_NAME
          , ANY_VALUE(HR.SUPERVISOR_NAME_1)                                         AS SUPERVISOR_NAME_1
-         , MIN(HR.CREATED_DATE)                                                    AS TEAM_START_DATE
-         , MAX(HR.EXPIRY_DATE)                                                     AS TEAM_EXPIRY_DATE
-         , ANY_VALUE(HR.HIRE_DATE)                                                 AS HIRE_DATE
-         , ANY_VALUE(HR.TERMINATION_DATE)                                          AS TERMINATION_DATE
          , DATEDIFF('MM', ANY_VALUE(HR.HIRE_DATE),
                     NVL(ANY_VALUE(HR.TERMINATION_DATE), CURRENT_DATE()))           AS MONTH_TENURE
+         , ANY_VALUE(HR.HIRE_DATE)                                                 AS HIRE_DATE
+         , ANY_VALUE(HR.TERMINATION_DATE)                                          AS TERMINATION_DATE
          , ANY_VALUE(HR.TERMINATION_CATEGORY)                                      AS TERMINATION_CATEGORY
          , ANY_VALUE(HR.TERMINATION_REASON)                                        AS TERMINATION_REASON
+         , MIN(HR.CREATED_DATE)                                                    AS TEAM_START_DATE
          -- Begin custom fields in the table
+         , CASE
+               WHEN MAX(HR.EXPIRY_DATE) >= CURRENT_DATE() AND ANY_VALUE(HR.TERMINATION_DATE) IS NOT NULL
+                   THEN ANY_VALUE(HR.TERMINATION_DATE)
+               ELSE MAX(HR.EXPIRY_DATE) END                                        AS TEAM_EXPIRY_DATE
          , ROW_NUMBER() OVER(PARTITION BY HR.EMPLOYEE_ID ORDER BY TEAM_START_DATE) AS RN
          , CASE
                WHEN ANY_VALUE(HR.SUPERVISORY_ORG) IN
@@ -26,17 +29,29 @@ WITH ENTIRE_HISTORY AS (
                ELSE FALSE END                                                      AS DIRECTOR_ORG
          , LEAD(DIRECTOR_ORG, 1, DIRECTOR_ORG) OVER(PARTITION BY HR.EMPLOYEE_ID
                 ORDER BY TEAM_START_DATE)                                          AS NEXT_DIRECTOR
+--          , CASE
+--                WHEN DIRECTOR_ORG = NEXT_DIRECTOR THEN FALSE
+-- --                WHEN DIRECTOR_ORG AND TERMINATION_DATE IS NOT NULL THEN TRUE
+--                ELSE TRUE END                                                       AS TRANSFER_OUT
          , CASE
-               WHEN DIRECTOR_ORG = NEXT_DIRECTOR THEN FALSE
-               WHEN DIRECTOR_ORG AND TERMINATION_DATE IS NOT NULL THEN TRUE
-               ELSE TRUE END                                                       AS TRANSFER_OUT
---     , NVL(ANY_VALUE(HR.TERMINATION_DATE),TEAM_EXPIRY_DATE) AS TRANSFER_OUT_DATE
+               WHEN ANY_VALUE(TERMINATION_DATE) >= TEAM_EXPIRY_DATE AND NOT NEXT_DIRECTOR THEN TRUE
+               WHEN DIRECTOR_ORG != NEXT_DIRECTOR THEN TRUE
+               ELSE FALSE END                                                      AS TRANSFER
     FROM HR.T_EMPLOYEE_ALL AS HR
+    WHERE HR.HIRE_DATE >= '2018-03-01'
     GROUP BY HR.EMPLOYEE_ID
            , HR.SUPERVISORY_ORG
     ORDER BY HR.EMPLOYEE_ID
-           , TEAM_START_DATE
+           , TEAM_START_DATE DESC
+)
+
+   , CORE_TABLE AS (
+    SELECT *
+    FROM ENTIRE_HISTORY
+    WHERE DIRECTOR_ORG
 )
 
 SELECT *
-FROM ENTIRE_HISTORY
+FROM CORE_TABLE
+ORDER BY EMPLOYEE_ID
+       , TEAM_START_DATE
