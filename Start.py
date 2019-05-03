44e75dfbbd00a2fe354423f891537a25b46506c0
4ndertron <robert.anderson@vivintsolar.com>
2019-05-03 07:06:47 -0600

    Added ER and Default root cause workbook queries and updated todos

--- Start.py
+++ Start.py
@@ -3,9 +3,6 @@ TODO: Initiative for paired programming. Think of the project and then coordinat
     Project Ideas:
         BBB API
         Smartsheet API
-
-TODO: Setup Default inflow root cause.
-TODO: Move the spreadsheet data sources to their own cube. They're preventing the queries from refreshing regularly.
 """
 
 # %% begin code
