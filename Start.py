cdf3f550e26f5e9214d97b75834d80bda142665c
4ndertron <robert.anderson@vivintsolar.com>
2019-05-16 06:52:22 -0600

    Updated todo's

--- Start.py
+++ Start.py
@@ -1,19 +1,8 @@
 """
 
-TODO: Filings workbook
 TODO: Business Review
 TODO: ECL Reporting
 
-TODO: Initiative for paired programming. Think of the project and then coordinate a time to screenshare.
-    Project Ideas:
-        BBB API
-
-TODO: Triple Trouble Automation
-    - New Account Batches
-    - Transfers Cases
-    - Continuations/Refiles
-TODO: ECL DASHBOARD
-TODO: Ratio of Total Installs on Priority Case Report
 TODO: Turn all Default WIP_END fields to CLOSED_DATE after collaboration with Brittany
 TODO: Update all Default reports with the new WIP_END Field.
 
@@ -21,32 +10,6 @@ TODO: BBB Inflow graph and attach what root cause tags that we do have for 2019.
 
 TODO: What is the cost, and what did they pay?
 
-Trust LD to do the filings. Not happening. Transfers is suffering.
-Agent's don't know where the account is in the filings process.
-Give them visibility into Luna.
-Just get done within 24 hours.
-In Progress, signal brittany to terminate filing. (Default or Transfer case?)
-Audit making sure there's TA, original.
-Put on spreadsheet & send to LD.
-Or they create the termination themselves.
-How do you share the workload, while having visibility.
-Transfer would be a comment. Release would be a task as SF.
-Or take out of Salesforce, into a google sheet.
-TODO: Open In Progress, pull out and into the sheet. Batch by time frames. Noon to 4 is LD. All else is VSLR.
-Split the batches from LD to filings.
-One tab is LD and one is Filings.
-LD marks an account complete in the bacth.
-Then the SF does not mark the complete.... Need to audit? Or fix?
-
-
-Team Priorities and outputs from priority case reviews.
--------------------------------------------------------
-#1) Priority Case Review Action Items Projects
-#2) CEO Case Analysis (Send out every Monday)
-#3) Weekly Risk Report
-#4) Priority Case Review Deck (Polish that) -- Me
-#5) Filings Account List by batch. -- Me (Transfers && Continuations/Refiles)
-#6) Filings Invoices & Uploads -- Me
 """
 
 # %% begin code
