e5367e6091892c4fdcc5d5cce2e29e327b1b5f71
4ndertron <robert.anderson@vivintsolar.com>
2019-06-05 09:09:24 -0600

    Updated todo's

--- Start.py
+++ Start.py
@@ -3,9 +3,9 @@
 TODO: Turn all Default WIP_END fields to CLOSED_DATE after collaboration with Brittany
 TODO: Update all Default reports with the new WIP_END Field.
 
-TODO: ECL Reporting
+TODO: Home Closing Date | Transaction Date
 
-TODO: What is the cost, and what did they pay?
+TODO:
 
 """
 
