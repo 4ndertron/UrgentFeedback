372466ddbdfbb89ef7941a7cc24f83a56da13822
4ndertron <robert.anderson@vivintsolar.com>
2019-06-06 15:11:07 -0600

    Updated requests, ad-hocs, and todo's

--- SQL/Filings/SepTransferCases.sql
+++ SQL/Filings/SepTransferCases.sql
@@ -9,8 +9,21 @@ WITH T1 AS (
          , S.SERVICE_COUNTY
          , S.SERVICE_STATE
          , S.SERVICE_ZIP_CODE
+         , CASE
+               WHEN C.RECORD_TYPE = 'Solar - Customer Default' AND
+                    C.CLOSED_DATE IS NOT NULL
+                   THEN TRUE END                                AS DEFAULT_BOOL
+         , CASE
+               WHEN C.RECORD_TYPE = 'Solar - Customer Escalation' AND
+                    C.CLOSED_DATE IS NOT NULL
+                   THEN TRUE END                                AS ER_BOOL
+         , CASE
+               WHEN C.RECORD_TYPE = 'Solar - Troubleshooting' AND
+                    C.CLOSED_DATE IS NOT NULL
+                   THEN TRUE END                                AS TS_BOOL
          , TO_DATE(C.HOME_CLOSING_DATE)                         AS TRANSACTION_DATE
          , DATEADD('MM', 246, TO_DATE(P.INSTALLATION_COMPLETE)) AS TERMINATION_DATE
+         , LDD.TOTAL_CURRENT_AMOUNT_DUE
     FROM RPT.T_CASE AS C
              LEFT JOIN
          RPT.T_PROJECT AS P
@@ -24,14 +37,18 @@ WITH T1 AS (
              LEFT JOIN
          RPT.T_CONTACT AS CTO
          ON CTO.CONTACT_ID = S.CONTRACT_CO_SIGNER
+             LEFT JOIN
+         LD.T_DAILY_DATA_EXTRACT AS LDD
+         ON LDD.BILLING_ACCOUNT = P.SOLAR_BILLING_ACCOUNT_NUMBER
     WHERE C.RECORD_TYPE = 'Solar - Transfer'
       AND C.CREATED_DATE >= '2018-09-01'
-      AND C.CLOSED_DATE BETWEEN DATEADD('D', -7, CURRENT_DATE) AND CURRENT_DATE -- 291
+      AND C.CLOSED_DATE < '2019-05-15'
+--       AND C.CLOSED_DATE BETWEEN DATEADD('D', -7, CURRENT_DATE) AND CURRENT_DATE -- 291
 --       AND C.CLOSED_DATE BETWEEN '2019-05-15' AND '2019-05-21' -- 289
 --       AND C.CLOSED_DATE BETWEEN '2019-05-22' AND '2019-05-28' -- 290
       AND C.STATUS = 'Closed - Processed'
       AND S.SERVICE_STATUS != 'Solar - Transfer'
-    ORDER BY C.CASE_NUMBER
+    ORDER BY TOTAL_CURRENT_AMOUNT_DUE DESC
 )
 
 SELECT *
--- Start.py
+++ Start.py
@@ -1,13 +1,15 @@
 """
+TODO: Create a Root Cause Google Form and subsequent sheet with a parser. Then make Cambree and Joel Buss the owners of
+ their respective forms and sheets.
 
-TODO: Turn all Default WIP_END fields to CLOSED_DATE after collaboration with Brittany
-TODO: Update all Default reports with the new WIP_END Field.
+TODO: Add london's money to paid in the risk management report.
+TODO: Add service charges.
 
-TODO: Home Closing Date | Transaction Date
+TODO: Add priority case lists to Root Cause Workbook.
 
-TODO:
-
+Austin from legal wants a pre-pto loan customer "is vslr's fault. tehy want to be reimbursing the loan customer's
+ their loan payemtns. Loan customer's that are 90 Day past install. What they're monthly loan payment is...
+ PRE-PTO 90 Day past install, loan accounts...
 """
 
 # %% begin code
-
