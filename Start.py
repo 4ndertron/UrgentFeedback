aef8de0bec58e28a749c25e58260685ccfa93ff7
robert.anderson <robert.anderson@vivintsolar.com>
2019-06-10 11:50:35 -0600

    Updated priority todo's

--- Start.py
+++ Start.py
@@ -1,4 +1,24 @@
 """
+
+TODO: SOP and Checklist for handling Filing Batch Issues.
+    What did Emily do?
+    Where did Emily keep the batch issues?
+    How should the current issues be handled?
+
+TODO: SOP and Checklist for handling monthly billing review items.
+    What does Jason/Alfonso do?
+    What does Liz have in documentation?
+    How many are done?
+    How many need to be done?
+    How many need to be fixed?
+
+
+TODO: MSTG Rep:
+    How many reps have accounts with default/escalation cases?
+    Take the escalation and default dashboards, then tag the sales reps on there.
+    Volume graph per rep.
+    Ratio graph per rep.
+
 TODO: Create a Root Cause Google Form and subsequent sheet with a parser. Then make Cambree and Joel Buss the owners of
  their respective forms and sheets.
 
